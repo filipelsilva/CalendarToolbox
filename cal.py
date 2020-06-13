58513c23a6eeb16ad7376ebbeb3dd52e0b99f66b
filipelsilva <57916727+filipelsilva@users.noreply.github.com>
2020-06-13 01:33:44 +0100

    adicao de comentarios

--- cal.py
+++ cal.py
@@ -3,25 +3,33 @@
 
 import sys
 
+# Valores do primeiro dia de cada mes e dos dias da semana
 primeiro_mes = "ADDGBEGCFADF"
 dias = ["domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado"]
 
+# Passa uma letra maiuscula para numero de 1 a 7
 def letra_para_num(letra):
     return ord(letra) - ord('A') + 1
 
-def translate(num):
+# Passa um numero de 1 a 7 para dia da semana
+def num_para_dia(num):
     print(dias[num - 1])
 
+# Escreve um dia formatado
 def print_dia(dia, mes, ano):
     print("Dia {}/{}/{}".format(dia, mes, ano))
 
+# Verifica se e bissexto
 def eh_bissexto(ano):
     if (ano == 2000):
         return True
 
+# Calcula o dia para o calendario gregoriano
 def calcula_gregoriano(dia, mes, ano):
+    print_dia(dia, mes, ano)
     return 0
 
+# Calcula o dia para o calendario juliano
 def calcula_juliano(dia, mes, ano):
     print_dia(dia, mes, ano)
     regressoes = (ano - 1) + (ano // 4)
@@ -32,8 +40,9 @@ def calcula_juliano(dia, mes, ano):
         w = 1 + (c - n + 6) % 7
     else:
         w = 1 + (c - n + 7) % 7
-    translate(w)
+    num_para_dia(w)
 
+# MAIN FUNCTION
 if __name__ == "__main__":
     c = sys.argv[1]
     d = int(sys.argv[2])
