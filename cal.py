2facfb615d357b919c1c03737fedfe6f0448409e
filipelsilva <57916727+filipelsilva@users.noreply.github.com>
2020-06-13 14:16:10 +0100

    adicao de resultados verbosos

--- cal.py
+++ cal.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 # Valores do primeiro dia de cada mes e dos dias da semana
 primeiro_mes = "ADDGBEGCFADF"
-dias = ["domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado"]
+dias = ["Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado"]
 
 # Passa uma letra maiuscula para numero de 1 a 7
 def letra_para_num(letra):
@@ -46,6 +46,8 @@ def calcula_dia_semana(escolha, dia, mes, ano):
     else:
         w = 1 + (c - n + 7) % 7
     # Apresentacao do resultado
+    print("Calculos auxiliares:\n\tRegressoes = {}\n\tn = {}\n\tr = {}\
+        \n\tc = {}\n\tw = {}".format(regressoes, n, r, c, w))
     num_para_dia(w)
 
 # Calcula o dia de pascoa de um ano
@@ -59,10 +61,11 @@ def calcula_dia_pascoa(escolha, ano):
         e0 = (57 + (11 * g) - (ano//100) + ((ano//100)//4) +
              (((ano//100) - (((ano//100) - 17)//25))//3))
         v = (e0/24 - e0/25) + (g/12) * (e0/25 - e0/26)
-        e = e0 + v
+        e = e0 + v # Epacta
     elif (escolha == "j"): # Calendario juliano
         regressoes = (ano + 4) + (ano//4)
         e = (11 * g - 3) % 30 # Epacta
+    # Restante calculo, comum a ambos os calendarios
     d = 20 + (54 - e) % 30
     c = 1 + (d + 2) % 7
     n = 7 - regressoes % 7
@@ -72,7 +75,11 @@ def calcula_dia_pascoa(escolha, ano):
         s = d + 7 - (c - n) % 7
     dia = datetime(ano, 3, 1)
     res = dia + timedelta(s - 1)
-    calcula_dia_semana(escolha, res.day, res.month, res.year)
+    # Apresentacao do resultado
+    print("Calculos auxiliares:\n\tg = {}\n\tRegressoes = {}\n\te0 = {}\
+        \n\tv = {}\n\te = {}\n\td = {}\n\tc = {}\n\tn = {}\n\ts = {}"
+        .format(g, regressoes, e0, v, e, d, c, n, s))
+    print_dia(res.day, res.month, res.year)
 
 # MAIN FUNCTION
 if __name__ == "__main__":
