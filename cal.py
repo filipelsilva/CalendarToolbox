2c360ef3e3a7f0ac49e565c8c571f188896e915d
filipelsilva <57916727+filipelsilva@users.noreply.github.com>
2020-06-13 01:45:46 +0100

    mais comentarios; calculo do gregoriano e dos bissextos

--- cal.py
+++ cal.py
@@ -21,13 +21,23 @@ def print_dia(dia, mes, ano):
 
 # Verifica se e bissexto
 def eh_bissexto(ano):
-    if (ano == 2000):
+    if ((ano % 400 == 0) or ((ano % 100 != 0) and (ano % 4 == 0))):
         return True
+    else:
+        return False
 
 # Calcula o dia para o calendario gregoriano
 def calcula_gregoriano(dia, mes, ano):
     print_dia(dia, mes, ano)
-    return 0
+    regressoes = (ano - 1) + (ano // 4) - (ano // 100) + (ano // 400)
+    n = 7 - regressoes % 7
+    r = letra_para_num(primeiro_mes[mes - 1]) + dia
+    c = 1 + (r - 2) % 7
+    if (eh_bissexto(ano) and mes in (1, 2)):
+        w = 1 + (c - n + 6) % 7
+    else:
+        w = 1 + (c - n + 7) % 7
+    num_para_dia(w)
 
 # Calcula o dia para o calendario juliano
 def calcula_juliano(dia, mes, ano):
@@ -36,7 +46,7 @@ def calcula_juliano(dia, mes, ano):
     n = 2 - regressoes % 7
     r = letra_para_num(primeiro_mes[mes - 1]) + dia
     c = 1 + (r - 2) % 7
-    if (eh_bissexto(ano) and mes not in (1, 2)):
+    if (eh_bissexto(ano) and mes in (1, 2)):
         w = 1 + (c - n + 6) % 7
     else:
         w = 1 + (c - n + 7) % 7
